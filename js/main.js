219a225884a8ed3d56e369a69cc2fce13713825e
dfigalo1 <daianafigalo@gmail.com>
2023-01-27 20:48:47 -0300

    terminando con shop

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ $('#myModal').on('shown.bs.modal', function () {
 $('.carousel').carousel({
   interval: 5000
 })
+
+
+const openModalBtn = document.querySelector('.open-modal');
+const modalContainer = document.querySelector('.modal-container');
+
+openModalBtn.addEventListener('click', () => {
+  modalContainer.classList.add('active');
+});
+
+modalContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal-background')) {
+    modalContainer.classList.remove('active');
+  }
+});
